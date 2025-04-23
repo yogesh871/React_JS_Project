eceea8ffbac921905f4e8b9a47a800d084529a85
yogesh <yogeshrd1708@gmail.com>
2025-04-23 08:51:26 +0530

    calculator_project_03

---
+++ calculator_project-03/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Calculator from './components/calculator'
+
+
+function App() {
+
+  return (
+    <>
+     <Calculator/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ calculator_project-03/src/components/calculator.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import './Calculator.css'; 
+
+const Calculator = () => {
+  const [Buttons, setButtons] = useState([ 1, 2, 3, '+', 4, 5, 6,'-', 7, 8, 9,'*', 0,  '/' ,'(', ')',' .', '%' ,  ]);
+  const [input, setInput] = useState('');
+
+  
+  const handleClick = (value) => {
+    setInput(prev => prev + value);
+  };
+
+  const handleClear = () => {
+    setInput('');
+  };
+
+  const handleButton = () => {
+      const result = eval(input);
+      setInput(result.toString());
+  };
+
+  
+  return (
+    <div className="calculator">
+      <input placeholder="0" type="text" value={input} />
+      <div className="button">
+        {Buttons.map((item, index) => (
+          <button class="btn" onClick={() => handleClick(item)} key={index}>
+            {item}
+          </button>
+        ))}
+        <button class="btn" onClick={handleButton}>=</button>
+        <button class="btn" onClick={handleClear}>C</button>
+      </div>
+
+    </div>
+  );
+};
+
+export default Calculator;
